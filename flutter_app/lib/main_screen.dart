d0b70ef1fc47b9d0add7c9216f4294bee0906c33
Dongmin <focuspdm4383@naver.com>
2025-01-21 13:02:50 +0900

    구글 계정으로 로그인 불러오기 성공 but 화면 다시구성필요

---
+++ flutter_app/lib/main_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black, // 검은 화면 배경
+      body: Center(
+        child: Text(
+          'Main Screen',
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
